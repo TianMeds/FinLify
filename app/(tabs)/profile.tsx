e14f36c910572de5791f6ad9d62d19802309c028
Christian Medallada <cbmedallada@student.apc.edu.ph>
2024-08-16 23:32:42 +0800

    Setup the File itself install nativewind and the add the assets and add the fonts and the icons but will apply new icons in the future that is needed

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <View>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+      <Text>NIGGA STILL NO WATER</Text>
+    </View>
+  )
+}
+
+export default Profile
---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+import icons from "./Icon";
+import colors from "./Colors";
+import images from "./Images";
+
+export { icons, colors, images };
